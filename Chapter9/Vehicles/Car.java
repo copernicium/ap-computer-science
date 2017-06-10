80320a62463d0ec5a6a9f4c2039cd0282da1086f
Logan <logan.traffas@gmail.com>
2017-06-10 02:49:56 -0700

    Add all computer science files

---
+++ Chapter9/Vehicles/Car.java
@@ -0,0 +1,13 @@
+package ComputerScience.Chapter9.Vehicles;
+
+/**
+ * @Author Logan Traffas
+ * @Date 3/1/2017.
+ * @Version 1.0.0
+ * @Assignment Ch 9: Introduction to Inheritance
+ */
+public class Car extends Vehicle{
+	public Car(){
+		super();
+	}
+}
